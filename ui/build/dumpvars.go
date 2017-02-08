fc2cac74cbaba9cd987b23fabbbe1aeed621a3c0
Abhisek Devkota <ciwrl@lineageos.org>
2017-02-08 12:06:13 -0800

    Add dumpvar for WITH_SU
     -Allow folks to see easily if the term session they are in has
       WITH_SU set to true
    
    Change-Id: Ib310d0016bb9eb16960f794720321b706b6fd8b0

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -116,6 +116,7 @@ var BannerVars = []string{
 	"TARGET_GCC_VERSION",
 	"TARGET_KERNEL_CLANG_VERSION",
 	"RECOVERY_TYPE",
+	"WITH_SU",
 }
 
 func Banner(make_vars map[string]string) string {
@@ -133,6 +134,7 @@ func Banner(make_vars map[string]string) string {
 	fmt.Fprintf(b, "%s=%s\n", "AICP_VERSION", make_vars["AICP_VERSION"])
 	fmt.Fprintf(b, "%s=%s\n", "BUILD_ID", make_vars["BUILD_ID"])
 	fmt.Fprintf(b, "%s=%s\n", "OUT_DIR", make_vars["OUT_DIR"])
+	fmt.Fprintf(b, "%s=%s\n", "WITH_SU", make_vars["WITH_SU"])
 	fmt.Fprintln(b, "=============================================================")
 	fmt.Fprintf(b, "%s=%s\n", "PLATFORM_VERSION_CODENAME", make_vars["PLATFORM_VERSION_CODENAME"])
 	fmt.Fprintf(b, "%s=%s\n", "PLATFORM_VERSION", make_vars["PLATFORM_VERSION"])
