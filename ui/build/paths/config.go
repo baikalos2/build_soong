c1c2a8ad37ac9a904e9a9f4996a7fea7c148bea1
Ali B <abittin@gmail.com>
2022-01-03 21:53:08 +0300

    soong: Whitelist mogrify
    Change-Id: I9d228fcf1153502df78280415ed364a9bb428451

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -97,6 +97,7 @@ var Configuration = map[string]PathConfig{
 	"locale":  Allowed,
 	"lsof":    Allowed,
 	"m4":      Allowed,
+	"mogrify": Allowed,
 	"nproc":   Allowed,
 	"openssl": Allowed,
 	"perl":    Allowed,
